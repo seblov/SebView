b88ec7d2b2c95756f4d8c28e40b61995239c33ba
Sebastian <lookeper2@gmail.com>
2023-03-17 15:42:10 +0100

    kinda fiksa query auto-update

--- app.js
+++ app.js
@@ -44,12 +44,12 @@
 //   console.log('Server started on port 3000');
 // });
 
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const sql = require('msnodesqlv8');
-const ejs = require('ejs');
-app.use(express.static('public'));
+const sql = require("msnodesqlv8");
+const ejs = require("ejs");
+app.use(express.static("public"));
 
 const connectionString = process.env.CONNECTION_STRING;
 
@@ -57,9 +57,9 @@ const connectionString = process.env.CONNECTION_STRING;
 const query1 = process.env.QUERY_1;
 const query2 = process.env.QUERY_2;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let results1, results2;
 
   // Execute the first query
@@ -81,17 +81,17 @@ app.get('/', (req, res) => {
       results2 = rows2;
 
       // Render the HTML template and pass the query results as variables
-      res.render('index', { results1, results2 });
+      res.render("index", { results1, results2 });
     });
   });
 });
 
 // Define a route to update query2
-app.get('/query2', (req, res) => {
+app.get("/query2", (req, res) => {
   sql.query(connectionString, query2, (err, rows) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Error executing query');
+      return res.status(500).send("Error executing query");
     }
 
     res.json(rows);
@@ -99,14 +99,14 @@ app.get('/query2', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 
   // Update query_2 every 5 minutes
   setInterval(updateQuery2, 5 * 60 * 1000);
 });
 
 function updateQuery2() {
-  fetch('/query2')
+  fetch('http://localhost:3000/query2')
     .then(response => response.json())
     .then(data => {
       const labels = data.map(result => result.admunit);
@@ -117,4 +117,3 @@ function updateQuery2() {
     })
     .catch(error => console.error(error));
 }
-
